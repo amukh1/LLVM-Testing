879d806905807b53c7c2e8dd1b0f9b8b80272385
Amukh1 <85003861+amukh1@users.noreply.github.com>
2023-09-06 22:02:31 -0400

    Update LLVMController.h

--- LLVMController.h
+++ LLVMController.h
@@ -6,8 +6,9 @@
 
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
-#include "llvm/IR/IRBuilder.h"
+#include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/Verifier.h>
+#include <llvm/IR/Instructions.h>
 #include <llvm/Support/raw_ostream.h>
 
 #include <iostream>
@@ -26,8 +27,22 @@ class LLVMController {
 
     LLVMController();
 
+    // functions
     llvm::Function* declareFunction(std::string name, llvm::FunctionType* type);
     llvm::BasicBlock* defineFunction(std::string name);
 
-    llvm::Value* getFromScope(std::string name);
+    void verifyFunction(llvm::Function& fn);
+    void callFunction(llvm::Function* fn, std::vector<llvm::Value*> args);
+
+    // llvm::Value* getFromScope(std::string name);
+
+    // variables
+    llvm::AllocaInst* declareVariable(std::string name, llvm::Type* type);
+    llvm::StoreInst* assignVariable(llvm::AllocaInst*, llvm::Value* value);
+    llvm::LoadInst* getVariable(llvm::AllocaInst* allocaInst, llvm::Type* type);
+
+    // arrays
+    llvm::AllocaInst* declareArray(std::string name, llvm::Type* type, llvm::Value* size);
+    llvm::StoreInst* assignArray(llvm::AllocaInst* allocaInst, llvm::Value* index, llvm::Value* value); // get index type from index->getType();
+    llvm::LoadInst* getArray(llvm::AllocaInst* allocaInst, llvm::Value* index, llvm::Type* type);
 };
